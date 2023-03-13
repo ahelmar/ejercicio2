328d2eed7f333efb40934f4fa432d0e8f8a76377
daniel <pedrodaniel.fernandezguerrero@alum.uca.es>
2023-03-13 13:25:54 +0100

    Agregar función mostrar_menu() y archivos menu.py y main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from menu import mostrar_menu
+
+def main():
+    numero = mostrar_menu()
+    print("El número ingresado es:", numero)
+
+if __name__ == '__main__':
+    main()
---
+++ menu.py
@@ -0,0 +1,9 @@
+def mostrar_menu():
+    print("1- sumar")
+    print("2- restar")
+    print("3- multiplicar")
+    print("4- dividir")
+    print("5- salir")
+    
+    numero = input("Ingrese un número: ")
+    return numero
